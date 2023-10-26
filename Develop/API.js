07bc110f95419af82015eba4de7784799a23fd72
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-25 21:50:28 -0700

    api module is writting successfully

--- Develop/API.js
+++ Develop/API.js
@@ -6,7 +6,7 @@ const allData = require('./db/db.json')
 
 
 router.get('/notes', (req,res) => { 
-  res
+  res.send(allData);
 })
 
 router.post('/notes', (req, res) => {
@@ -17,15 +17,20 @@ router.post('/notes', (req, res) => {
         text,
     };
 
-    fs.readFile('Develop/db/db.json', 'utf8', (error, data) => {
+    fs.readFile("./db/db.json", 'utf8', (error, data) => {
       if(error) {
         console.log(error);
       } else {
         const parsedNotes = JSON.parse(data);
         parsedNotes.push(newNote);
+        console.log(newNote + "New Note Added to API list ")
       
-        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes,))
-
+        fs.writeFile("./db/db.json", JSON.stringify(parsedNotes,null ,4),
+        (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+              )
       };
     })
   };
