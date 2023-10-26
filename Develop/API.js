cc48d4f2ca55fdce6a96b6cbc132f23112a879b7
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-25 22:30:43 -0700

    api can be overwritten

--- Develop/API.js
+++ Develop/API.js
@@ -1,14 +1,26 @@
+// Import Require Modules
 const fs = require('fs');
 const express = require('express')
 const router = express.Router()
 const allData = require('./db/db.json')
 
 
-
+// GET request for existing notes
 router.get('/notes', (req,res) => { 
-  res.send(allData);
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err.status);
+      // return res.status(500).json('Error in posting review');
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+      res.status(200).json(parsedNotes);
+    }
+  });
 })
 
+// POST request for adding notes
 router.post('/notes', (req, res) => {
   const { title, text } = req.body;
   if(title && text) {
@@ -16,13 +28,14 @@ router.post('/notes', (req, res) => {
         title,
         text,
     };
-
+    // Read the json file and add new comment to that file
     fs.readFile("./db/db.json", 'utf8', (error, data) => {
       if(error) {
         console.log(error);
       } else {
         const parsedNotes = JSON.parse(data);
         parsedNotes.push(newNote);
+        allData.push(newNote);
         console.log(newNote + "New Note Added to API list ")
       
         fs.writeFile("./db/db.json", JSON.stringify(parsedNotes,null ,4),
--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,26 @@
+// Import requires modules
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3001;
 const api = require('./API')
 
+
+// Use Express 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', api);
 
+
+// Setting Route for HTML
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+
+
+// Setting Route for Note
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
