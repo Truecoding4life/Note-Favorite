6af7c1b7b0e34913d5899a3bb551559a9504903f
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-27 17:16:59 -0700

    PORT FIXED

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 const api = require("./API"); // <- Express Router API
 
 // Use Express
